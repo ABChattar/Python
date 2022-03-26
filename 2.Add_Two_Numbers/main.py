ca4a24db2913ee17f5218866680aacfffa420f72
Akshay Chattar <48723164+ABChattar@users.noreply.github.com>
2022-03-26 23:21:46 +0530

    Add files via upload

---
+++ 2.Add_Two_Numbers/main.py
@@ -0,0 +1,10 @@
+Num_1 = 10
+Num_2 = 20
+
+print(f"The Addition of {Num_1} and {Num_2} = {Num_1 + Num_2}")
+print(f"The Subtraction  of {Num_1} and {Num_2} = {Num_1 - Num_2}")
+print(f"The Multiplication of {Num_1} and {Num_2} = {Num_1 * Num_2}")
+print(f"The Division of {Num_1} and {Num_2} = {Num_1 / Num_2}")
+print(f"The Division of {Num_1} and {Num_2} = {Num_1 // Num_2}") # float results
+print(f"The Power of {Num_1} to the {Num_2} = {Num_1 ** Num_2}")
+print(f"The Modulus of {Num_1} % {Num_2} = {Num_1 % Num_2}")
